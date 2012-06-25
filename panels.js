00f0d1500155cce9336528bfe0cc5a444a7c45b5
Alec Guintu <aeguintu@gmail.com>
2012-06-25 16:10:20 -0700

    Break apart and stuff.

--- panels.js
+++ panels.js
@@ -1,15 +1,15 @@
 var originalImages = [];
-var newImages = [];
+var allImages = [];
 
 $(window).load(function() {
   var nx = 5;
-  var ny = 5;
+  var ny = 4;
 
   var margin = 10;
   var xmid = parseInt(nx / 2);
   var ymid = parseInt(ny / 2);
 
-  var mainImage = $("#getme");
+  var mainImage = $(".gallery img:first-child");
   var imagesWidth = mainImage.width() / nx;
   var imagesHeight = mainImage.height() / ny;
 
@@ -17,36 +17,46 @@ $(window).load(function() {
   var divLeft = miPositions.left;
   var divTop = miPositions.top;
 
-  for (y = 0; y < ny; y++) {
-    for (x = 0; x < nx; x++) {
-      var gotImage = mainImage.clone().removeAttr('id');
-      gotImage.attr('style', 'position:relative');
-      gotImage.css('left', (-(imagesWidth * x))+'px').css('top', (-(imagesHeight * y))+'px');
+  $(".gallery img").hide();
+  $.each($(".gallery img"), function(index, value) {
+    var thisImage = $(value);
+    allImages.push(breakApart(thisImage));
+  });
 
-      var div = $('<div style="position:absolute; overflow:hidden">');
-      div.css('width', imagesWidth+'px').css('height', imagesHeight+'px');
-      div.css('left', (divLeft + (imagesWidth * x))+'px').css('top', (divTop + (imagesHeight * y))+'px');
+  returnBack(allImages[2]);
 
-      div.css('left', (divLeft + (imagesWidth * x) - (margin * (xmid - x))));
-      div.css('top', (divTop + (imagesHeight * y) - (margin * (ymid - y))));
+  function copyImage() {
 
-      div.append(gotImage);
-      $("body").append(div);
-    }
   }
 
-  mainImage.hide();
+  function breakApart(image) {
+    var j = [];
+
+    for (y = 0; y < ny; y++) {
+      for (x = 0; x < nx; x++) {
+        var gotImage = image.clone().removeAttr('id');
+        gotImage.attr('style', 'position:relative');
+        gotImage.css('left', (-(imagesWidth * x))+'px').css('top', (-(imagesHeight * y))+'px');
+
+        var div = $('<div style="position:absolute; overflow:hidden">');
+        div.css('width', imagesWidth+'px').css('height', imagesHeight+'px');
+        div.css('left', (divLeft + (imagesWidth * x))+'px').css('top', (divTop + (imagesHeight * y))+'px');
+
+        div.css('left', (divLeft + (imagesWidth * x) - (margin * (xmid - x))));
+        div.css('top', (divTop + (imagesHeight * y) - (margin * (ymid - y))));
+
+        div.append(gotImage);
+
+        j.push(div);
+      }
+    }
+
+    return j;
+  }
+
+  function returnBack(images) {
+    for (i = 0; i < images.length; i++) {
+      $("body").append(images[i]);
+    }
+  }
 });
-
-
-function copyImage() {
-  
-}
-
-function breakApart() {
-  
-}
-
-function returnBack() {
-  
-}
