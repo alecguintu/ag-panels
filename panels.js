16cdcdc8d9ee9d8f61f1714db436a0421052f6f8
Alec Guintu <aeguintu@gmail.com>
2012-07-01 18:25:32 -0700

    Created into a jQuery plugin.

--- panels.js
+++ panels.js
@@ -1,62 +1,169 @@
-var originalImages = [];
-var allImages = [];
+// Check boiler plate jQuery plugin from:
+// http://stefangabos.ro/jquery/jquery-plugin-boilerplate-revisited/
 
-$(window).load(function() {
-  var nx = 5;
-  var ny = 4;
+(function($) {
 
-  var margin = 10;
-  var xmid = parseInt(nx / 2);
-  var ymid = parseInt(ny / 2);
+  $.panels = function(element, options) {
+    var defaults = {
+      rows: 5,
+      cols: 5,
+      margin: 10,
+      someCallback: function() {}
+    }
 
-  var mainImage = $(".gallery img:first-child");
-  var imagesWidth = mainImage.width() / nx;
-  var imagesHeight = mainImage.height() / ny;
+    var panel = this;
+    panel.settings = {};
 
-  var miPositions = mainImage.position();
-  var divLeft = miPositions.left;
-  var divTop = miPositions.top;
+    var $container = $(element), $images = $(element).children('img'), element = element;
+    var rowMid, colMid, brokenWidth, brokenHeight, posLeft, posTop;
 
-  $(".gallery img").hide();
-  $.each($(".gallery img"), function(index, value) {
-    var thisImage = $(value);
-    allImages.push(breakApart(thisImage));
-  });
+    panel.init = function() {
+      panel.settings = $.extend({}, defaults, options);
 
-  returnBack(allImages[2]);
+      var thisImage = get_first();
 
-  function copyImage() {
+      rowMid = parseInt(panel.settings.rows / 2);
+      colMid = parseInt(panel.settings.cols / 2);
 
-  }
+      brokenWidth = thisImage.width() / panel.settings.rows;
+      brokenHeight = thisImage.height() / panel.settings.cols;
 
-  function breakApart(image) {
-    var j = [];
+      posLeft = thisImage.position().left;
+      posTop = thisImage.position().top;
 
-    for (y = 0; y < ny; y++) {
-      for (x = 0; x < nx; x++) {
-        var gotImage = image.clone().removeAttr('id');
-        gotImage.attr('style', 'position:relative');
-        gotImage.css('left', (-(imagesWidth * x))+'px').css('top', (-(imagesHeight * y))+'px');
+      x = break_apart(thisImage);
+      return_back(x);
 
-        var div = $('<div style="position:absolute; overflow:hidden">');
-        div.css('width', imagesWidth+'px').css('height', imagesHeight+'px');
-        div.css('left', (divLeft + (imagesWidth * x))+'px').css('top', (divTop + (imagesHeight * y))+'px');
+      $images.hide();
 
-        div.css('left', (divLeft + (imagesWidth * x) - (margin * (xmid - x))));
-        div.css('top', (divTop + (imagesHeight * y) - (margin * (ymid - y))));
+      panel.next();
+    }
 
-        div.append(gotImage);
+    var get_first = function() {
+      return $container.children('img').filter(':first');
+    }
 
-        j.push(div);
+    var break_apart = function(image) {
+      var j = [];
+
+      for (y = 0; y < panel.settings.cols; y++) {
+        for (x = 0; x < panel.settings.rows; x++) {
+          var gotImage = image.clone();
+          gotImage.attr('style', 'position:relative');
+          gotImage.css('left', (-(brokenWidth * x))+'px').css('top', (-(brokenHeight * y))+'px');
+
+          var div = $('<div style="position:absolute; overflow:hidden">');
+          div.css('width', brokenWidth+'px').css('height', brokenHeight+'px');
+          div.css('left', (posLeft + (brokenWidth * x))+'px').css('top', (posTop + (brokenHeight * y))+'px');
+
+          div.css('left', (posLeft + (brokenWidth * x) - (panel.settings.margin * (rowMid - x))));
+          div.css('top', (posTop + (brokenHeight * y) - (panel.settings.margin * (colMid - y))));
+
+          div.append(gotImage);
+
+          j.push(div);
+        }
+      }
+
+      return j;
+    }
+
+    var return_back = function(images) {
+      for (i = 0; i < images.length; i++) {
+        $("body").append(images[i]);
       }
     }
 
-    return j;
+    panel.play = function() {
+      console.log('play');
+    }
+    panel.next = function() {
+      console.log('next');
+    }
+    panel.previous = function() {
+      console.log('previous');
+    }
+
+    // Here we go!
+    panel.init();
   }
 
-  function returnBack(images) {
-    for (i = 0; i < images.length; i++) {
-      $("body").append(images[i]);
-    }
+  $.fn.panels = function(options) {
+    return this.each(function() {
+      if (undefined == $(this).data('panels')) {
+        var panel = new $.panels(this, options);
+
+        $(this).data('panels', panel);
+      }
+    });
   }
+
+})(jQuery);
+
+$(window).load(function() {
+  $(".gallery").panels({rows:7, cols:4, margin:15});
 });
+
+
+// var originalImages = [];
+// var allImages = [];
+// 
+// $(window).load(function() {
+//   var nx = 5;
+//   var ny = 2;
+// 
+//   var margin = 10;
+//   var xmid = parseInt(nx / 2);
+//   var ymid = parseInt(ny / 2);
+// 
+//   var mainImage = $(".gallery img:first-child");
+//   var imagesWidth = mainImage.width() / nx;
+//   var imagesHeight = mainImage.height() / ny;
+// 
+//   var miPositions = mainImage.position();
+//   var divLeft = miPositions.left;
+//   var divTop = miPositions.top;
+// 
+//   $(".gallery img").hide();
+//   $.each($(".gallery img"), function(index, value) {
+//     var thisImage = $(value);
+//     allImages.push(breakApart(thisImage));
+//   });
+// 
+//   returnBack(allImages[0]);
+// 
+//   function copyImage() {
+// 
+//   }
+// 
+//   function breakApart(image) {
+//     var j = [];
+// 
+//     for (y = 0; y < ny; y++) {
+//       for (x = 0; x < nx; x++) {
+//         var gotImage = image.clone().removeAttr('id');
+//         gotImage.attr('style', 'position:relative');
+//         gotImage.css('left', (-(imagesWidth * x))+'px').css('top', (-(imagesHeight * y))+'px');
+// 
+//         var div = $('<div style="position:absolute; overflow:hidden">');
+//         div.css('width', imagesWidth+'px').css('height', imagesHeight+'px');
+//         div.css('left', (divLeft + (imagesWidth * x))+'px').css('top', (divTop + (imagesHeight * y))+'px');
+// 
+//         div.css('left', (divLeft + (imagesWidth * x) - (margin * (xmid - x))));
+//         div.css('top', (divTop + (imagesHeight * y) - (margin * (ymid - y))));
+// 
+//         div.append(gotImage);
+// 
+//         j.push(div);
+//       }
+//     }
+// 
+//     return j;
+//   }
+// 
+//   function returnBack(images) {
+//     for (i = 0; i < images.length; i++) {
+//       $("body").append(images[i]);
+//     }
+//   }
+// });
